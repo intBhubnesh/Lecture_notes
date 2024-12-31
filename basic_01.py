9dcccbfa791a37bd906523aa6a983c08cb8e8ddb
intBhubnesh <bhubnesh2002@gmail.com>
2024-12-31 10:10:46 +0530

    inital commit

---
+++ basic_01.py
@@ -0,0 +1,21 @@
+# list : Lists are used to store multiple items in a single variable.
+list = ['apple', 'banana', 'oraneg']
+
+# Tuple : A tuple is a collection which is ordered and unchangeable.
+tuple = ('10','20','30')
+
+# set : A set is a collection which is unordered, unchangeable*, and unindexed.
+set = {'maths', 'english', 'hindi', 'physics'}
+
+# dictionary : A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+dict ={
+    'maths' : 10,
+    'english' : 20,
+    'hindi' : 30,
+    'physics' : 40,
+}
+
+print('list : ',list[0])
+print('tuple : ',tuple[0])
+print('set : ',set)
+print('dictionary : ',dict['maths'])
